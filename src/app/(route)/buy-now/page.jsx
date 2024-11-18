25293c19e71842343d75fd5703e87c7fc509afe6
chapman91 <chapmandoneil91@gmail.com>
2024-11-18 08:06:02 -0500

    - Add API key

--- src/app/(route)/buy-now/page.jsx
+++ src/app/(route)/buy-now/page.jsx
@@ -1,6 +1,15 @@
-import React from 'react'
+'use client';
+
+import { useEffect } from 'react';
+import styles from "../../styles/buy-now.module.css";
+import React from 'react';
 
 const BuyNowPage = () => {
+
+  useEffect(() => {
+
+  })
+
   return (
     <div>BuyNowPage</div>
   )
