849abd0520a7a74c29d1f17541a25e9aee508156
chapman91 <chapmandoneil91@gmail.com>
2024-12-06 13:47:57 -0500

    -revision 5th

--- src/app/components/componentsBarrel.tsx
+++ src/app/components/componentsBarrel.tsx
@@ -1,6 +1,5 @@
 // Components Barrel.jsx
 
-export { default as WalletConnectButton } from './atoms/client/connectWalletButton';
 export { default as NormalButton } from './atoms/client/normalButton';
 export { default as YellowButton } from './atoms/client/yellowButton';
 export { default as TokenomicsCard } from './molecules/server/TokenomicsCard';
@@ -9,6 +8,5 @@ export { default as NavigationBar } from './organisms/client/navigationbar';
 export { default as Footer } from './organisms/client/Footer';
 export { default as WalletContext } from './providers/Context';
 export { ConnectionContext } from './providers/Context';
-export { default as DefaultSwap } from './organisms/client/DefaultSwap';
 
 export { default as ChakraProviderWrapper } from './providers/ChakraProviderWrapper';
