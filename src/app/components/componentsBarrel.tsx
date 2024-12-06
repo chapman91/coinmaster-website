ed3a825561b16067f549d7a9197f5b9a2c3dbe30
chapman91 <chapmandoneil91@gmail.com>
2024-12-06 14:19:12 -0500

    removed barrel import paths that failed Netlify build

--- src/app/components/componentsBarrel.tsx
+++ src/app/components/componentsBarrel.tsx
@@ -6,7 +6,5 @@ export { default as TokenomicsCard } from './molecules/server/TokenomicsCard';
 export { default as Modals } from './molecules/server/Modals';
 export { default as NavigationBar } from './organisms/client/navigationbar';
 export { default as Footer } from './organisms/client/Footer';
-export { default as WalletContext } from './providers/Context';
-export { ConnectionContext } from './providers/Context';
 
 export { default as ChakraProviderWrapper } from './providers/ChakraProviderWrapper';
